6dc834f51bd9ff62b249062f9f9c8bd8e3d8bfca
孙利飞 <sunlifei@126.com>
2026-06-18 10:31:39 +0800

    提交

---
+++ static/js/vodData.js
@@ -0,0 +1,142 @@
+const vodData = [
+	{
+		src: 'https://alimov2.a.kwimgs.com/upic/2024/07/22/21/BMjAyNDA3MjIyMTU4NTJfMjIwODUxMjM3OF8xMzg1MTEzMjM3OTdfMV8z_b_B66b123892046741aa3fba5aa79961a5b.mp4?clientCacheKey=3xc5ci6wg99pcf2_b.mp4&tt=b&di=88a55fd&bp=10000',
+		coverImg: 'https://p4.a.yximgs.com/upic/2024/07/22/21/BMjAyNDA3MjIyMTU4NTJfMjIwODUxMjM3OF8xMzg1MTEzMjM3OTdfMV8z_B0b7bbd54aee7591ab150553286ebf17a.jpg?clientCacheKey=3xc5ci6wg99pcf2.jpg&di=88a55fd&bp=10000',
+		object_fit: 'cover',
+		fullScreenShow: false,
+		userName: '',
+		video_title: '一定要理想型吗 她不行吗',
+		video_sound: ''
+	},
+	{
+		src: 'https://partyanimals.cn/media/videos/1.mp4',
+		coverImg: '/static/image/draft_cover1.jpg',
+		object_fit: 'contain',
+		fullScreenShow: true,
+		userName: '@卧槽无情',
+		video_title: '开发不易，如果对你有帮助，请给个五星好评！开发不易，如果对你有帮助，请给个五星好评！',
+		video_sound: '卧槽无情的原声'
+	},
+	{
+		src: 'https://partyanimals.cn/media/videos/2.mp4',
+		coverImg: '/static/image/draft_cover2.jpg',
+		object_fit: 'contain',
+		fullScreenShow: true,
+		userName: '@卧槽无情',
+		video_title: '开发不易，如果对你有帮助，请给个五星好评！开发不易，如果对你有帮助，请给个五星好评！',
+		video_sound: '卧槽无情的原声',
+		unlock: 1
+	},
+	{
+		src: 'https://txmov2.a.kwimgs.com/upic/2024/12/11/12/BMjAyNDEyMTExMjQ1NTVfMzkyODQ5MDA4MV8xNTA4NzYyMDQ1MTNfMl8z_b_Bae081028c088999b8289c3bf975c0ede.mp4?clientCacheKey=3xv5dfcgk8hbbkq_b.mp4&tt=b&di=88a55fd&bp=10000',
+		coverImg: 'https://p1.a.yximgs.com/upic/2024/12/11/12/BMjAyNDEyMTExMjQ1NTVfMzkyODQ5MDA4MV8xNTA4NzYyMDQ1MTNfMl8z_Bcfd952fa1c483f6e5d05a475d2cd91a0.jpg?clientCacheKey=3xv5dfcgk8hbbkq.jpg&di=88a55fd&bp=10000',
+		object_fit: 'cover',
+		fullScreenShow: false,
+		userName: '',
+		video_title: '艾特你那野猪兄弟来品细糠#这样的身材有人喜欢吗 #你拿什么抵抗我 #这谁顶得住啊',
+		video_sound: ''
+	},
+	{
+		src: 'https://partyanimals.cn/media/videos/3.mp4',
+		coverImg: '/static/image/draft_cover3.jpg',
+		object_fit: 'contain',
+		fullScreenShow: true,
+		userName: '@卧槽无情',
+		video_title: '开发不易，如果对你有帮助，请给个五星好评！开发不易，如果对你有帮助，请给个五星好评！',
+		video_sound: '卧槽无情的原声'
+	},
+	{
+		src: 'https://txmov2.a.kwimgs.com/upic/2024/04/17/16/BMjAyNDA0MTcxNjU5MDRfNDY1MTUwOTU2XzEzMDA3OTQ3OTAxNV8xXzM=_b_B7e6ced8175e05ca0f11263314972551c.mp4?clientCacheKey=3x3w5wpqnvqh49u_b.mp4&tt=b&di=88a55fd&bp=10000',
+		coverImg: 'https://p1.a.yximgs.com/upic/2024/04/17/16/BMjAyNDA0MTcxNjU5MDRfNDY1MTUwOTU2XzEzMDA3OTQ3OTAxNV8xXzM=_Bab99c0a413f2cc583bd39acdc6ed544e.jpg?clientCacheKey=3x3w5wpqnvqh49u.jpg&di=88a55fd&bp=10000',
+		object_fit: 'contain',
+		fullScreenShow: false,
+		userName: '',
+		video_title: '#丝滑小连招舞 #丝滑小连招   看你干的好事..咳咳～#反差',
+		video_sound: ''
+	},
+	{
+		src: 'https://partyanimals.cn/media/videos/4.mp4',
+		coverImg: '/static/image/draft_cover4.jpg',
+		object_fit: 'contain',
+		fullScreenShow: true,
+		userName: '@卧槽无情',
+		video_title: '开发不易，如果对你有帮助，请给个五星好评！开发不易，如果对你有帮助，请给个五星好评！',
+		video_sound: '卧槽无情的原声'
+	},
+	{
+		src: 'https://partyanimals.cn/media/videos/5.mp4',
+		coverImg: '/static/image/draft_cover5.jpg',
+		object_fit: 'contain',
+		fullScreenShow: true,
+		userName: '@卧槽无情',
+		video_title: '开发不易，如果对你有帮助，请给个五星好评！开发不易，如果对你有帮助，请给个五星好评！',
+		video_sound: '卧槽无情的原声'
+	},
+	{
+		src: 'https://txmov2.a.kwimgs.com/upic/2024/11/16/19/BMjAyNDExMTYxOTA3NThfMzc1NzQ0MzY4NV8xNDg3ODEyNTUyMDFfMF8z_b_B83ab204ff347b9c32c570a566b8987cc.mp4?clientCacheKey=3xdcfepqqz9t5cy_b.mp4&tt=b&di=88a55fd&bp=10000',
+		coverImg: 'https://p1.a.yximgs.com/upic/2024/11/16/19/BMjAyNDExMTYxOTA3NThfMzc1NzQ0MzY4NV8xNDg3ODEyNTUyMDFfMF8z_B3c6964e58fde9a0c9da3301faa628ab9.jpg?clientCacheKey=3xdcfepqqz9t5cy.jpg&di=88a55fd&bp=10000',
+		object_fit: 'cover',
+		fullScreenShow: false,
+		userName: '',
+		video_title: '这个歌有点暧昧 #气质清纯美女  #身材与颜值共存  #养眼美女',
+		video_sound: '',
+		unlock: 1
+	},
+	{
+		src: 'https://alimov2.a.kwimgs.com/upic/2024/10/24/18/BMjAyNDEwMjQxODUwNTBfMzcxNTE5MzExXzE0Njg0OTMzMDQ0N18xXzM=_b_B81e2660befe50b4a00f0935af462a9f8.mp4?clientCacheKey=3xwm8a34a3eyfiy_b.mp4&tt=b&di=88a55fd&bp=10000',
+		coverImg: 'https://p3.a.yximgs.com/upic/2024/10/24/18/BMjAyNDEwMjQxODUwNTBfMzcxNTE5MzExXzE0Njg0OTMzMDQ0N18xXzM=_Bf25eb66598fcb41ec1d7541758c9d2f1.jpg?clientCacheKey=3xwm8a34a3eyfiy.jpg&di=88a55fd&bp=10000',
+		object_fit: 'cover',
+		fullScreenShow: false,
+		userName: '',
+		video_title: '我身材这么好  不这么穿可惜了',
+		video_sound: ''
+	},
+	{
+		src: 'https://alimov2.a.kwimgs.com/upic/2024/04/22/17/BMjAyNDA0MjIxNzA3MjFfMzU1ODI4MDA2MF8xMzA1MTAzNTc2ODVfMV8z_b_B2b438be8c0b7b5d37c6cd2ca15a177e4.mp4?clientCacheKey=3xyjuw5dxtwtnzq_b.mp4&tt=b&di=88a55fd&bp=10000',
+		coverImg: 'https://p2.a.yximgs.com/upic/2024/04/22/17/BMjAyNDA0MjIxNzA3MjFfMzU1ODI4MDA2MF8xMzA1MTAzNTc2ODVfMV8z_B58313e59df414825cc661dc5e0f19925.jpg?clientCacheKey=3xyjuw5dxtwtnzq.jpg&di=88a55fd&bp=10000',
+		object_fit: 'contain',
+		fullScreenShow: false,
+		userName: '',
+		video_title: '不可以 #甜妹',
+		video_sound: ''
+	},
+	{
+		src: 'https://alimov2.a.kwimgs.com/upic/2024/10/31/16/BMjAyNDEwMzExNjE2NDBfMTMyNTM0ODQwN18xNDc0MDk5NjcyNzdfMV8z_b_B9436ba0d8ab69efa9098875c90b4e593.mp4?clientCacheKey=3x527wjs5ssxxya_b.mp4&tt=b&di=88a55fd&bp=10000',
+		coverImg: 'https://p2.a.yximgs.com/upic/2024/10/31/16/BMjAyNDEwMzExNjE2NDBfMTMyNTM0ODQwN18xNDc0MDk5NjcyNzdfMV8z_Bdcb3b9dc539a3fb043b7ef1c78787b5c.jpg?clientCacheKey=3x527wjs5ssxxya.jpg&di=88a55fd&bp=10000',
+		object_fit: 'contain',
+		fullScreenShow: false,
+		userName: '',
+		video_title: '门一开，我愣了一下#情侣日常 #内容过于真实 #电梯门突然开了',
+		video_sound: ''
+	},
+	{
+		src: 'https://alimov2.a.kwimgs.com/upic/2022/11/03/03/BMjAyMjExMDMwMzE3MDZfNzU1ODQzOTVfODc4NDAwMDk1MTlfMl8z_b_Ba6d94d8bde4c92034db94b7672f2805c.mp4?clientCacheKey=3xwfa3qgme3htbs_b.mp4&tt=b&di=88a55fd&bp=10000',
+		coverImg: 'https://p2.a.yximgs.com/upic/2022/11/03/03/BMjAyMjExMDMwMzE3MDZfNzU1ODQzOTVfODc4NDAwMDk1MTlfMl8z_B782e06bcb1bcb44f088ed70d97ccd672.jpg?clientCacheKey=3xwfa3qgme3htbs.jpg&di=88a55fd&bp=10000',
+		object_fit: 'contain',
+		userName: '',
+		video_title: '蚊子杀手 #纯欲风 #拍蚊子舞',
+		video_sound: ''
+	},
+	{
+		src: 'https://alimov2.a.kwimgs.com/upic/2023/02/09/15/BMjAyMzAyMDkxNTQzNDFfMTQ4NzQ0MDk0OV85NTgxOTYwNDQwM18xXzM=_b_B19e8a058d610b39f09967406643eddb9.mp4?clientCacheKey=3xqck4iiedb93nk_b.mp4&tt=b&di=88a55fd&bp=10000',
+		coverImg: 'https://p2.a.yximgs.com/upic/2023/02/09/15/BMjAyMzAyMDkxNTQzNDFfMTQ4NzQ0MDk0OV85NTgxOTYwNDQwM18xXzM=_B4cffcb68b1f63b613fa1d37c69fdd257.jpg?clientCacheKey=3xqck4iiedb93nk.jpg&di=88a55fd&bp=10000',
+		object_fit: 'contain',
+		fullScreenShow: false,
+		userName: '',
+		video_title: '假如你现在的电量 是你姐妹脱单的年纪 #卡点 #变装',
+		video_sound: ''
+	},
+	{
+		src: 'https://alimov2.a.kwimgs.com/upic/2024/11/14/18/BMjAyNDExMTQxODQxMzBfMTk0ODk1Nzc0M18xNDg1OTEzODk2NTFfMV8z_b_Bbbf12dd0b13b1421a39167343b390391.mp4?clientCacheKey=3xqcmf3x4nbf4xc_b.mp4&tt=b&di=88a55fd&bp=10000',
+		coverImg: 'https://p4.a.yximgs.com/upic/2024/11/14/18/BMjAyNDExMTQxODQxMzBfMTk0ODk1Nzc0M18xNDg1OTEzODk2NTFfMV8z_B1836538a88211d6bb3600fba958581b9.jpg?clientCacheKey=3xqcmf3x4nbf4xc.jpg&di=88a55fd&bp=10000',
+		object_fit: 'cover',
+		fullScreenShow: false,
+		userName: '',
+		video_title: '@窝是吉吉',
+		video_sound: ''
+	},
+]
+export const getVodData = () =>{
+	return vodData
+}
+// export default vodData
